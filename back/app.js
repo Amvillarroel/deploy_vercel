8dfe4906a7345ca417136eaad047afae476287de
Amnel <amvillarroel@gmail.com>
2023-11-16 23:31:38 -0300

    'cambio campo contrasena'

--- back/app.js
+++ back/app.js
@@ -23,7 +23,7 @@ const dbConnection = mysql.createPool({
 
 
 app.post('/login', (req, res) =>{
-    const sql = 'SELECT * FROM login_table WHERE Usuario = ? AND Contraseña = ?';
+    const sql = 'SELECT * FROM login_table WHERE Usuario = ? AND Contrasena = ?';
     
     dbConnection.query(sql, [req.body.email, req.body.password], (err, data) =>{
         if(err)  return res.json("Error de conexión de la BD");
