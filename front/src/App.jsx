cb23db95d5a30ee555254aeaea8c1848f8598fb9
Carlos Soto <carloss.empire@gmail.com>
2023-11-27 23:49:34 -0300

    estilos con css para el header

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { Header } from './components/Header';
+import { Header } from './components/Header/Header.jsx';
 import Login from './components/Login';
 import Home from './components/Home';
 import Details from './components/Details';
